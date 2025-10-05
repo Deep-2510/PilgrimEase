e2980744c3d4d6704ecf11261f16649370c87444
Deep-2510 <dp302817@gmail.com>
2025-10-05 12:22:19 +0530

    commit the files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import './index.css';
 
 function App() {
   return (
+    
     <AppProvider>
       <Router>
         <div className="app">
@@ -22,6 +23,11 @@ function App() {
               <Route path="/temple/:id" element={<TempleView />} />
               <Route path="/emergency" element={<Emergency />} />
               <Route path="/profile" element={<Profile />} />
+              <div>
+      <h1>🚀 Temple Crowd Management System</h1>
+      <p>Welcome to the Temple Crowd Management application!</p>
+      <p>If you can see this, React is working correctly.</p>
+    </div>
               <Route path="/admin" element={<Admin />} />
             </Routes>
           </main>
